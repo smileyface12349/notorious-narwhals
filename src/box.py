ab5a489d2ced47a97c6525d46ed5710534f7887d
Artem <artem30801@gmail.com>
2021-07-11 12:06:17 +0300

    Extracted methods in BoxState

--- src/box.py
+++ src/box.py
@@ -16,7 +16,7 @@ class BoxState:
 
     def clear(self) -> NoReturn:
         """Clears the box of all objects"""
-        self.objects = []
+        self.objects = []  # todo use queue or deque to keep objects z-sorted efficiently
 
     def add_object(self, added_object) -> NoReturn:
         """Adds an object to the objects list"""
@@ -28,32 +28,42 @@ class BoxState:
             return  # There is nothing to render!
 
         for obj in sorted(self.objects, key=lambda x: x.z):  # Sorts objects by their Z depth
-            if obj.override_colors:  # If the object decides to override default property-based colors:
-                curses.init_pair(100, *obj.color)
-                color = curses.color_pair(100)
-            else:
-                # We're sorting if statements by importance. An object being bouncy is the most important,
-                # therefore we're making it the top if statement.
+            self._render_object(obj, screen)
 
-                if obj.elasticity >= 0.6:  # If the object is bouncy
-                    color = curses.color_pair(4)
-                elif obj.friction == 1:  # If an object has a friction of 1, it's sticky.
-                    color = curses.color_pair(2)
-                elif obj.friction <= 0.3:
-                    color = curses.color_pair(3)
-                else:  # If it's just a regular ol' object.
-                    color = curses.color_pair(1)
+        # screen.update() ?
 
-            # Drawing stuff on the screen character-by-character
-            # It doesn't affect performance because curses draws all the stuff at once
-            # when screen.update() is called (or when .getkey() / .getch() is called)
-            for x in range(obj.size[0]):
-                for y in range(obj.size[1]):
-                    draw_pos_x = obj.position[0] + x
-                    draw_pos_y = obj.position[1] + y
+    def _render_object(self, obj, screen) -> NoReturn:
+        """Renders object on the screen (character-by-character)"""
+        # Drawing stuff on the screen character-by-character
+        # It doesn't affect performance because curses draws all the stuff at once
+        # when screen.update() is called (or when .getkey() / .getch() is called)
+        color = self._get_object_color(obj)
 
-                    # Giant if statement to decide if we should draw the tile
-                    if (draw_pos_x >= 0 and draw_pos_x < screen.getmaxyx()[1]) and (
-                        draw_pos_y >= 0 and draw_pos_y < screen.getmaxyx()[0]
-                    ):
-                        screen.insch(draw_pos_y, draw_pos_x, "@", color)
+        for x in range(obj.size[0]):
+            for y in range(obj.size[1]):
+                draw_pos_x = obj.position[0] + x
+                draw_pos_y = obj.position[1] + y
+
+                # Giant if statement to decide if we should draw the tile
+                if (draw_pos_x >= 0 and draw_pos_x < screen.getmaxyx()[1]) and (
+                    draw_pos_y >= 0 and draw_pos_y < screen.getmaxyx()[0]
+                ):
+                    screen.insch(draw_pos_y, draw_pos_x, "@", color)
+
+    @staticmethod
+    def _get_object_color(obj) -> int:
+        """"""
+        if obj.override_colors:  # If the object decides to override default property-based colors:
+            curses.init_pair(100, *obj.color)
+            return curses.color_pair(100)
+
+        # We're sorting if statements by importance. An object being bouncy is the most important,
+        # therefore we're making it the top if statement.
+        if obj.elasticity >= 0.6:  # If the object is bouncy
+            return curses.color_pair(4)
+        elif obj.friction == 1:  # If an object has a friction of 1, it's sticky.
+            return curses.color_pair(2)
+        elif obj.friction <= 0.3:  # Icy
+            return curses.color_pair(3)
+        else:  # If it's just a regular ol' object.
+            return curses.color_pair(1)
