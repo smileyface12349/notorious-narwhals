4f6a858e97f0241101e10d497ef5e2b6fa4c8ae9
smileyface12349 <josh.humphriss.signup@gmail.coom>
2021-07-12 15:53:45 +0100

    Changed datatypes.py into a package to make adding new data types easier

--- src/datatypes/__init__.py
+++ src/datatypes/__init__.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
 
+from .vector import Vector  # noqa: F401
+
 Position = namedtuple("Position", ["x", "y"])
 Size = namedtuple("Size", ["width", "height"])
 Rectangle = namedtuple("Rectangle", ["x1", "y1", "x2", "y2"])
