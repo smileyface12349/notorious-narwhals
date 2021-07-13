b6548d9210232d740fc4acb62256efe37c83aa55
awollgar <awollgarten@posteo.net>
2021-07-13 21:27:26 +0200

    finished setter and getting methods for game_object

--- src/game_object.py
+++ src/game_object.py
@@ -9,17 +9,20 @@ from datatypes import Vector
 class StaticBody:
     """represents the properties of a shape object"""
 
-    def __init__(self, position: Vector, shape: int, texture: int, orientation: float = 0):
-        """Init a new static body object
+    def __init__(self, position: Vector, shape: int, texture: int, size: Vector = (1, 1), orientation: float = 0):
+        """Init of a static body object
 
         Args:
-            position (Vector): [description]
-            shape ([type]): [description]
-            orientation ([type]): [description]
-            texture ([type]): [description]
+            position (Vector): position of object where units are in tiles
+            shape (int): shape type for object, int corresponds to enum for shape
+            texture (int): texture type for object, int corresponds to texture type
+            size (Vector, optional): Size of object, position corresponds to the upper-left of the object.
+            Defaults to (1, 1).
+            orientation (float, optional): Clockwise rotation from directly upwards in degrees. Defaults to 0.
         """
         self.position = position
         self.shape = shape
+        self.size = size
         self.orientation = orientation
         self.texture = texture
 
@@ -31,6 +34,10 @@ class StaticBody:
         """Returns shape of object"""
         return self.shape
 
+    def get_size(self) -> Vector:
+        """Returns size of object"""
+        return self.size
+
     def get_orientation(self) -> float:
         """Returns orientation of object"""
         return self.orientation
@@ -39,7 +46,7 @@ class StaticBody:
         """Returns texture of object"""
         return self.texture
 
-    def get_body_parameters(self) -> list[Vector, int, float, int]:
+    def get_body_parameters(self) -> "list[Vector, int, float, int]":
         """Returns body parameters"""
         return [self.position, self.shape, self.orientation, self.texture]
 
@@ -47,12 +54,13 @@ class StaticBody:
         """Sets position of object"""
         self.position = position
 
+    def set_size(self, size: Vector) -> NoReturn:
+        """Sets size of object"""
+        self.size = size
+
 
 class KinematicBody(StaticBody):
     """Represents the physics of a dynamic body"""
-
-    # Physical constants
-    MIN_BOUNCE_VELOCITY = 2
 
     def __init__(
         self,
@@ -60,66 +68,106 @@ class KinematicBody(StaticBody):
         shape: int,
         texture: int,
         mass: float,
+        z: int = 0,
+        collision: int = 1,
         orientation: float = 0,
         elasticity: float = 0,
         friction: float = 0,
-        gravity_magnitude: float = 0,
+        velocity: Vector = (0, 0),
+        gravity: Vector = (0, -0.49),
+        size: Vector = (1, 1),
+        forces: list = [],
+        # triggers: 'list[float, float, float, int]' = [],
     ):
-        """Init a new dynamic body object
+        """[summary]
 
         Args:
-            position (Vector): [description]
-            shape ([type]): [description]
-            orientation ([type]): [description]
-            texture ([type]): [description]
-            mass (float): [description]
-            elasticity (float, optional): [description]. Defaults to 0.
-            friction (float, optional): [description]. Defaults to 0.
-            gravity_magnitude (float, optional): [description]. Defaults to 0.
+            position (Vector): position of object where units are in tiles
+            shape (int): shape type for object, int corresponds to enum for shape
+            texture (int): texture type for object, int corresponds to texture type
+            mass (float): in kg, determines the amount of exerted force
+            z (int, optional): Determines which object is displayed on top. Defaults to 0.
+            collision (int, optional):
+            orientation (float, optional): Clockwise rotation from directly upwards in degrees. Defaults to 0.
+            elasticity (float, optional): The bouncyness of an object, value determines the
+            proportion of kinetic energy conserved. Defaults to 0.
+            friction (float, optional): Friction coeff. Defaults to 0.
+            z (int, optional): Determines which object is displayed on top. Defaults to 0.
+            velocity (Vector, optional): How fast an object is moving. Defaults to (0, 0).
+            gravity (Vector, optional): Acceleration of freefall. Defaults to (0, -0.49).
+            size (Vector, optional): Size of object, Defaults to to (1,1).
+            forces (list, optional): list of external forces that object has. Defaults to [].
+
+
         """
-        super().__init__(position=position, shape=shape, texture=texture, orientation=orientation)
+        super().__init__(position=position, shape=shape, texture=texture, orientation=orientation, size=size)
         self.mass = mass
+        self.z = z
         self.elasticity = elasticity
         self.friction = friction
-        self.gravity_magnitude = gravity_magnitude
-        self.accel_x: float = 0
-        self.accel_y: float = 0
+        self.velocity = velocity
+        self.gravity = gravity
+        self.forces = forces
+        # self.triggers = triggers
+        self.collision = collision
 
-    def accelerate(self, magnitude_x: float, magnitude_y: float) -> NoReturn:
-        """Accelerates the dynamic body"""
-        self.accel_x += magnitude_x
-        self.accel_y += magnitude_y
+    def get_mass(self) -> float:
+        """Returns mass of object"""
+        return self.mass
 
-    def apply_force(self, force_x: float, force_y: float) -> NoReturn:
-        """Apply a force to the dynamic body"""
-        self.accel_x += force_x * self.mass
-        self.accel_y += force_y * self.mass
+    def get_z(self) -> int:
+        """Returns z of object"""
+        return self.z
 
-    def apply_gravity(self) -> NoReturn:
-        """Apply gravity to the dynamic body"""
-        self.accelerate(0, self.gravity_magnitude)
+    def get_elasticity(self) -> float:
+        """Returns elasticity of object"""
+        return self.elasticity
 
-    def apply_friction_x(self, x_velo: float, prev_x: float, temp_friction_x: float) -> NoReturn:
-        """Not done yet"""
-        # TODO: implement friction formulas: Friction(max) = normal reaction * friction coefficient and logic
-        # governing friction inheritance
-        pass
+    def get_friction(self) -> float:
+        """Returns friction of object"""
+        return self.friction
 
-    def apply_friction_y(self, y_velo: float, prev_y: float, temp_friction_y: float) -> NoReturn:
-        """Not done yet"""
-        # TODO: implement friction formulas: Friction(max) = normal reaction * friction coefficient and logic
-        # governing friction inheritance
-        pass
+    def get_velocity(self) -> Vector:
+        """Returns velocity of object"""
+        return self.velocity
 
-    def get_x_force(self) -> float:
-        """Get the force on the x axis"""
-        return self.accel_x * self.mass
+    def get_gravity(self) -> Vector:
+        """Returns gravity of object"""
+        return self.gravity
 
-    def get_y_force(self) -> float:
-        """Get the force on the y axis"""
-        return self.accel_y * self.mass
+    def get_forces(self) -> Vector:
+        """Returns forces of object"""
+        return self.forces
 
-    def update(self, *args) -> NoReturn:
-        """Not done yet"""
-        # TODO: logic to update objects acceleration and velocities
-        pass
+    # def get_triggers(self) -> list:
+    #     """Returns triggers of object"""
+    #     return self.triggers
+
+    def get_collision(self) -> int:
+        """Returns collision group of object"""
+        return self.collision
+
+    def get_velocity_magnitude(self) -> int:
+        """Returns velocity magnitude of object"""
+        return Vector.magnitude(self.velocity)
+
+    def get_momentum(self) -> float:
+        """Returns the momentum of object"""
+        return Vector.magnitude(self.velocity) * self.mass
+
+    def set_z(self, z: int) -> NoReturn:
+        """Sets value of z of object"""
+        self.z = z
+
+    def set_velocity(self, velocity: Vector) -> NoReturn:
+        """Sets velocity of object"""
+        self.velocity = velocity
+
+    def set_forces(self, forces: list) -> NoReturn:
+        """Sets value of forces object"""
+        self.forces = forces
+
+    def process_trigger(self, trigger: list) -> NoReturn:
+        """Sets value of processes triggers for object"""
+        if trigger is not None:
+            pass
