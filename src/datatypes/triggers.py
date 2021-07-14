a82488a404366bbcb21fd00f748784a1ec56add8
smileyface12349 <josh.humphriss.signup@gmail.coom>
2021-07-14 16:17:12 +0100

    Added basic data structure to store triggers. This allows objects to interact with other objects and do more complicated things like completing the level. Other types of trigger can easily be subclassed from Trigger

---
+++ src/datatypes/triggers.py
@@ -0,0 +1,53 @@
+from typing import List, NoReturn
+
+from src.datatypes.game_object import GameObject
+
+from .vector import Vector
+
+
+class Triggers:
+    """Represents an output action for a sensor (e.g. button, lever)
+
+    Consists of a list of triggers
+    """
+
+    def __init__(self, triggers: List["Trigger"]):
+        self.triggers = triggers
+
+    def process(self) -> NoReturn:
+        """Process a list of triggers (in order)"""
+        for trigger in self.triggers:
+            trigger.process()
+
+
+class Trigger:
+    """Represents a single trigger"""
+
+    def process(self) -> NoReturn:
+        """Process the trigger. Must be subclassed"""
+        pass
+
+
+class ForceTrigger(Trigger):
+    """Represents a trigger involving applying a force to another object"""
+
+    def __init__(self, target: GameObject, force: Vector):
+        self.target = target
+        self.force = force
+
+    def process(self) -> NoReturn:
+        """Process a force-based trigger"""
+        self.target.add_temp_force(self.force)
+
+
+class CustomTrigger(Trigger):
+    """Represents a trigger with no direct action
+
+    The .process() method should be defined manually when initializing the class
+
+    Where possible, it is recommended to create an additional class to provide the functionality required
+    """
+
+    def __init__(self, identifier: int, **kwargs):
+        self.identifier = identifier
+        self.options = kwargs
