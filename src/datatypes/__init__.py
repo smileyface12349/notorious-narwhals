c08e7a3e016fda519b3c3dffa7722faabb7b05e3
smileyface12349 <josh.humphriss.signup@gmail.coom>
2021-07-14 11:26:16 +0100

    Imported some types of textures

--- src/datatypes/__init__.py
+++ src/datatypes/__init__.py
@@ -7,6 +7,10 @@ Edges = namedtuple("Corners", ["left", "top", "right", "bottom"])
 Color = namedtuple("Color", ["fg", "bg"])
 Menu = namedtuple("Menu", ["text_lines", "options"])
 
+from .textures import EmptyTexture as EmptyTextureImported  # noqa: E402
+from .textures import Texture as TextureImported  # noqa: E402
 from .vector import Vector as VectorImported  # noqa: E402
 
 Vector = VectorImported
+Texture = TextureImported
+EmptyTexture = EmptyTextureImported
