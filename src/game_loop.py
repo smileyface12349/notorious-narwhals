cbaff131bc87c879f16c49bee8484ef9c621cab1
Artem <artem30801@gmail.com>
2021-07-15 15:19:23 +0300

    Refactored Menu

--- src/game_loop.py
+++ src/game_loop.py
@@ -1,9 +1,11 @@
 import curses
+import enum
 import os
 import time
-from typing import NoReturn
+from abc import ABC
+from typing import NoReturn, Optional
 
-from datatypes import Menu, Size
+from datatypes import Menu
 from input_getter import InputGetter
 from window_manager import WindowManager
 
@@ -13,100 +15,149 @@ def center(string: str, width: int) -> str:
     return f"{string:^{width}}"
 
 
-class MenuDrawer:
-    """A class to draw menus"""
+class ExitCodes(enum.IntEnum):
+    """Return codes used in the AbstractAppLoop to indicate pause or app exit"""
 
-    def __init__(
-        self, screen: curses.window, window_manager: WindowManager, input_getter: InputGetter, max_fps: int = 20
-    ):
-        """Initialize a new menu"""
-        self.screen = screen
-        self.window_manager = window_manager
-        self.input_getter = input_getter
-        self.width, self.height = os.get_terminal_size()
-        curses.init_pair(200, curses.COLOR_BLACK, curses.COLOR_WHITE)
-        self.window_manager.update()
-        self.max_fps = max_fps
-
-    def update(self) -> NoReturn:
-        """Update width and height"""
-        self.width, self.height = os.get_terminal_size()
-
-    def draw_menu(self, menu: Menu) -> int:
-        """Draw a menu"""
-        max_length = max(list(map(lambda x: len(x), menu.text_lines + menu.options)))
-        self.window_manager.min_size = Size(
-            max_length * self.window_manager.font_size.width,
-            (len(menu.text_lines) + len(menu.options) + 3) * self.window_manager.font_size.height,
-        )
-        self.window_manager.update()
-        top_line = int(self.height / 2 - (len(menu.text_lines) + len(menu.options) + 3) / 2)
-        selected = 0
-        while True:
-            self.window_manager.update()
-            self.update()
-            self.screen.clear()
-            for index, item in enumerate(menu.text_lines):
-                self.screen.addstr(top_line + index + 1, 0, center(item, self.width))
-            for index, item in enumerate(menu.options):
-                if index == selected:
-                    self.screen.attron(curses.color_pair(200))
-                self.screen.addstr(top_line + len(menu.text_lines) + index + 2, 0, center(item, self.width))
-                if index == selected:
-                    self.screen.attroff(curses.color_pair(200))
-            self.screen.refresh()
-
-            time.sleep(1 / self.max_fps)
-            key = self.input_getter.get_first_char_index(remove=True)
-            if key == 10:
-                return selected
-            elif key == curses.KEY_UP:
-                selected = max(selected - 1, 0)
-            elif key == curses.KEY_DOWN:
-                selected = min(selected + 1, len(menu.options) - 1)
-            elif key in [ord("q"), ord("Q")]:
-                return -1
+    PAUSE = -2
+    STOP = -1
 
 
-class GameLoop:
-    """Main game loop class. Entry point of the game."""
+class AbstractAppLoop(ABC):
+    """Application loop class for curses"""
+
+    PAUSE_KEYS = {27, ord("p"), ord("P")}
+    STOP_KEYS = {3, 26, ord("q"), ord("Q")}
 
     def __init__(self, window_manager: WindowManager, input_getter: InputGetter, max_fps: int = 20):
         self.max_fps = max_fps
         self.window_manager = window_manager
         self.input_getter = input_getter
-        self.active_state_box = None
 
         self.running = False
+        self.return_code = None
         self.throttle = False
 
-    def start(self) -> NoReturn:
+    def start(self) -> Optional[int]:
         """Main game loop. This method blocks until game is finished!"""
         period = 1 / self.max_fps
         self.running = True
+        exit_code = None
 
         self._pre_loop()
         while self.running:
             frame_end_time = time.time() + period
-            self._loop_step()
+
+            exit_code = self._loop_step()
+            if exit_code is not None:
+                break
+
             sleep_time = max(0.0, frame_end_time - time.time())
             self.throttle = sleep_time == 0
             time.sleep(sleep_time)
 
-            key = self.input_getter.get_first_char_index(remove=True)
-            if key in [27, ord("p"), ord("P")]:
-                return -2
-            elif key in [ord("q"), ord("Q")]:
-                return -1
+        self._post_loop()
+        return exit_code
 
     def _pre_loop(self) -> NoReturn:
         """Every call that is to be scheduled before loop start goes here"""
         self.window_manager.update()  # for correct population of previous frame
 
-    def _loop_step(self) -> NoReturn:
+    def _post_loop(self) -> NoReturn:
+        """Every call that is to be scheduled after loop stop goes here"""
+        pass
+
+    def _loop_step(self) -> Optional[int]:
         """Every call that is to be scheduled at each frame goes here"""
         self.window_manager.update()
-        self.active_state_box.render()
+        key = self.input_getter.get_first_char_index(remove=True)
+        exit_code = self._get_key_action(key)
+        return exit_code
+
+    def _get_key_action(self, key: int) -> int:
+        """Returns exit code or action of pressed key"""
+        if key in self.PAUSE_KEYS:
+            return ExitCodes.PAUSE
+        elif key in self.STOP_KEYS:
+            curses.endwin()
+            return ExitCodes.STOP
+
+
+class GameLoop(AbstractAppLoop):
+    """Main game loop class. Entry point of the game."""
+
+    def _loop_step(self) -> Optional[int]:
+        """Every call that is to be scheduled at each frame goes here"""
+        exit_code = super()._loop_step()
+        self.window_manager.update()
+        # self.active_state_box.render()
+        return exit_code
+
+
+class MenuLoop(AbstractAppLoop):
+    """Menu loop class. Handles both rendering and processing of the menu"""
+
+    MENU_COLOR_PAIR = 200
+
+    def __init__(
+        self, screen: curses.window, window_manager: WindowManager, input_getter: InputGetter, max_fps: int = 20
+    ):
+        self.screen = screen
+        self.menu = None
+        self.selected_index = 0
+        curses.init_pair(self.MENU_COLOR_PAIR, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        super().__init__(window_manager=window_manager, input_getter=input_getter, max_fps=max_fps)
+
+    def show_menu(self, menu: Menu) -> Optional[int]:
+        """Displays specified menu"""
+        self.menu = menu
+        return self.start()
+
+    def _pre_loop(self) -> NoReturn:
+        """Every call that is to be scheduled before loop start goes here"""
+        super()._pre_loop()
+
+    def _loop_step(self) -> Optional[int]:
+        """Every call that is to be scheduled at each frame goes here"""
+        exit_code = super()._loop_step()
+        self.screen.clear()
+
+        rows, cols = self.screen.getmaxyx()
+        # self.screen.resize(rows, cols)
+        curses.resize_term(rows, cols)  # more window glitches, less text glitches and crashes
+
+        top_line = rows // 2 - (len(self.menu.text_lines) + len(self.menu.options)) // 2
+        for index, item in enumerate(self.menu.text_lines):
+            y = top_line + index + 1
+            x = cols // 2 - len(item) // 2
+            if len(item) < cols and y < rows - 1:
+                self.screen.addstr(y, x, item)
+
+        for index, item in enumerate(self.menu.options):
+            color = curses.color_pair(0) if index != self.selected_index else curses.color_pair(self.MENU_COLOR_PAIR)
+            y = top_line + len(self.menu.text_lines) + index + 2
+            x = cols // 2 - len(item) // 2
+
+            if len(item) < cols and y < rows - 1:  # checks aren't working sometimes?
+                self.screen.addstr(y, x, item, color)
+
+        self.screen.refresh()
+
+        return exit_code
+
+    def _post_loop(self) -> NoReturn:
+        """Every call that is to be scheduled after loop stop goes here"""
+        self.screen.clear()
+
+    def _get_key_action(self, key: int) -> int:
+        """Returns exit code or action of pressed key"""
+        if key == 10:
+            return self.selected_index
+        elif key == curses.KEY_UP:
+            self.selected_index = max(self.selected_index - 1, 0)
+        elif key == curses.KEY_DOWN:
+            self.selected_index = min(self.selected_index + 1, len(self.menu.options) - 1)
+
+        return super()._get_key_action(key)
 
 
 # fmt: off
@@ -114,26 +165,32 @@ menus = {
     "start": Menu(
         ["Title of the Game", "By the Notorious Narwhals"],
         ["Play", "Select Level", "How to play", "About", "Settings", "Exit"],
+        ["loop", "levels", "help", "about", "settings", ExitCodes.STOP],
     ),
     "levels": Menu(
         ["Not implemented yet"],
-        ["Back"]
+        ["Back"],
+        ["start"],
     ),
     "help": Menu(
         ["Paste here a nice explanation", "how to play the game"],
-        ["Back"]
+        ["Back"],
+        ["start"],
     ),
     "about": Menu(
         ["Paste here a nice text about", "the Notorious Narwhals and the Code Jam"],
-        ["Back"]
+        ["Back"],
+        ["start"],
     ),
     "settings": Menu(
         ["Not implemented yet"],
-        ["Back"]
+        ["Back"],
+        ["start"],
     ),
     "pause": Menu(
         ["Paused"],
-        ["Continue", "Settings", "Menu", "Exit"]
+        ["Continue", "Settings", "Menu", "Exit"],
+        ["loop", "settings", "start", ExitCodes.STOP]
     )
 }
 # fmt: on
@@ -146,44 +203,23 @@ def main(screen: curses.window) -> NoReturn:
 
     window_manager = WindowManager()
     input_getter = InputGetter(screen)
-    menu_drawer = MenuDrawer(screen, window_manager, input_getter)
+    menu_drawer = MenuLoop(screen, window_manager, input_getter)
     loop = GameLoop(window_manager, input_getter)
 
     menu = "start"
     while True:
-        menu_return = menu_drawer.draw_menu(menus[menu])
-        if menu_return == -1:
+        menu_return = menu_drawer.show_menu(menus[menu])
+        if menu_return == ExitCodes.STOP:  # exit via keyboard
             break
-        if menu == "start":
-            if menu_return == 0:
-                menu = "loop"
-            elif menu_return == 1:
-                menu = "levels"
-            elif menu_return == 2:
-                menu = "help"
-            elif menu_return == 3:
-                menu = "about"
-            elif menu_return == 4:
-                menu = "settings"
-            elif menu_return == 5:
-                break
-        elif menu in ["levels", "help", "about", "settings"]:
-            menu = "start"
-        elif menu == "pause":
-            if menu_return == 0:
-                menu = "loop"
-            elif menu_return == 1:
-                menu = "settings"
-            elif menu_return == 2:
-                menu = "start"
-            elif menu_return == 3:
-                break
+        menu = menu_drawer.menu.options_actions[menu_return]
+        if menu == ExitCodes.STOP:  # exit via option action choice
+            break
 
         if menu == "loop":
             loop_return = loop.start()
-            if loop_return == -1:
+            if loop_return == ExitCodes.STOP:
                 break
-            elif loop_return == -2:
+            elif loop_return == ExitCodes.PAUSE:
                 menu = "pause"
 
     input_getter.quit()
