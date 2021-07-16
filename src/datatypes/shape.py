413f9e218d30cc679fbf7dcba8719bbe0f236c90
Martysh12 <martyshkon@gmail.com>
2021-07-16 21:10:47 +0300

    Added a Shape enum

---
+++ src/datatypes/shape.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class Shape(Enum):
+    """The shape of an object"""
+
+    Rectangle = 0
+    Line = 1
+    Circle = 2
+    Triangle = 3  # TODO: Create a triangle renderer
