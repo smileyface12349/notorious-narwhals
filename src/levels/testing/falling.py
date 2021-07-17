f3e771687fe7f2ae1365ed9712b9c1cc6b8cc5d3
smileyface12349 <josh.humphriss.signup@gmail.coom>
2021-07-17 12:06:43 +0100

    Assigned a higher z value to the falling object

--- src/levels/testing/falling.py
+++ src/levels/testing/falling.py
@@ -15,7 +15,7 @@ obj = FallingObject(position=Vector(relative_x=0.5, relative_y=0.5), collision=[
 obj.texture = SolidTexture(char="O", colour=1, obj=obj)
 objects.append(obj)
 
-obj = Wall(position=Vector(relative_x=0.4, relative_y=0.8), size=Vector(relative_x=0.2, y=1), collision=[1])
+obj = Wall(position=Vector(relative_x=0.4, relative_y=0.8), size=Vector(relative_x=0.2, y=1), collision=[1], z=1)
 obj.texture = SolidTexture(char="_", colour=0, obj=obj)
 objects.append(obj)
 
