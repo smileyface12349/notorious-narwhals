4af8fb93e9f3c366dacdf440fa4fb910cc824670
Martysh12 <49569238+Martysh12@users.noreply.github.com>
2021-09-25 13:33:46 +0300

    Removing textures in falling.py

--- src/levels/testing/falling.py
+++ src/levels/testing/falling.py
@@ -7,16 +7,13 @@ sys.path.append("..")
 
 from src.box import BoxState  # noqa: E402
 from src.datatypes import Vector  # noqa: E402 F401
-from src.datatypes.textures import SolidTexture  # noqa: E402 F401
 
 objects = []
 
-obj = FallingObject(position=Vector(relative_x=0.5, relative_y=0.5), collision=[1], z=1, elasticity=0.5)
-obj.texture = SolidTexture(char="O", colour=1, obj=obj)
+obj = FallingObject(position=Vector(relative_x=0.5, relative_y=0.5), collision=[1], z=1, elasticity=0.5, char="O", colour=1)
 objects.append(obj)
 
-obj = Wall(position=Vector(relative_x=0.4, relative_y=0.8), size=Vector(relative_x=0.2, y=1), collision=[1])
-obj.texture = SolidTexture(char="_", colour=0, obj=obj)
+obj = Wall(position=Vector(relative_x=0.4, relative_y=0.8), size=Vector(relative_x=0.2, y=1), collision=[1], char="_", colour=0)
 objects.append(obj)
 
 level = BoxState(initial_objects=objects)
